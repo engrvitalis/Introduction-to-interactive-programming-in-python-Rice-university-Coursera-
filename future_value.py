6fca98797baadb62774077a247d713675514fd3e
engrvitalis <ndlinks@gmail.com>
2021-01-07 11:42:06 +0100

    Initial commit

--- future_value.py
+++ future_value.py
@@ -1,44 +1,21 @@
-# Compute the circumference of a circle, given the length of its radius.
+def future_value(present_value, annual_rate, periods_per_year, years):
+    """
+When investing money, an important concept to know is compound interest.  
 
-###################################################
-# Tests
-# Student should uncomment ONLY ONE of the following at a time.
-PI = 3.14
+The equation   FV = PV(1 + rate)**periods
+  Where:
+    The present value (PV) of your money is how much money you have now. 
+    The future value (FV) of your money is how much money you will have in the future.
+    The nominal interest rate per period (rate) is how much interest you earn during a 
+    particular length of time, before accounting for compounding.  This is typically 
+    expressed as a percentage.
+    The number of periods (periods) is how many periods in the future this calculation is for.
+"""
+    rate_per_period = annual_rate / periods_per_year
+    periods = periods_per_year * years
 
-# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
-radius = 8
+    return (present_value * (1 + rate_per_period) ** periods)
 
-
-# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
-#radius = 3
-
-
-# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
-#radius = 12.9
-
-
-###################################################
-# Circle circumference formula
-# Student should enter formula on the next line.
-
-circumference = 2 * PI * radius
-
-###################################################
-# Test output
-# Student should not change this code.
-
-print("A circle with a radius of " + str(radius) + " inches has a circumference of " + str(circumference) + " inches.")
+print ("$1000 at 2% compounded daily for 3 years yields $", round(future_value(1000, .02, 365, 3), 4))
 
 
-###################################################
-# Expected output
-# Student should look at the following comments and compare to printed output.
-
-# Test 1 output:
-#A circle with a radius of 8 inches has a circumference of 50.24 inches.
-
-# Test 2 output:
-#A circle with a radius of 3 inches has a circumference of 18.84 inches.
-
-# Test 3 output:
-#A circle with a radius of 12.9 inches has a circumference of 81.012 inches.
